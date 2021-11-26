34e172c845f9f3d83f7e461fb555612a31fe6d73
ofoe-fiergbor <ofoefiergbor16@gmail.com>
2021-11-26 19:53:18 +0000

    initial commit

---
+++ src/main/java/com/group19/tpdiscoveryservice/TpDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.group19.tpdiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group19/tpdiscoveryservice/TpDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group19.tpdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
