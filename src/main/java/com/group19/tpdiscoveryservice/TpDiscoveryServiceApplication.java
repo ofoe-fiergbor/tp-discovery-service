b3542b8c8d7591ffe2027103a93382794e396ade
ofoe-fiergbor <ofoefiergbor16@gmail.com>
2021-11-30 12:27:28 +0000

    implemented discovery server

--- src/main/java/com/group19/tpdiscoveryservice/TpDiscoveryServiceApplication.java
+++ src/main/java/com/group19/tpdiscoveryservice/TpDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.group19.tpdiscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class TpDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
