581e3d4ad151c2921a3401b6d211977b37194838
michaelalabi1 <michael.alabi-duobu@amalitech.com>
2021-12-06 21:47:43 +0000

    setting up eureka service

--- src/main/java/com/group19/tpdiscoveryservice/TpDiscoveryServiceApplication.java
+++ src/main/java/com/group19/tpdiscoveryservice/TpDiscoveryServiceApplication.java
@@ -3,13 +3,20 @@ package com.group19.tpdiscoveryservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class TpDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpDiscoveryServiceApplication.class, args);
+	}
+	@RequestMapping(value = "/")
+	public String home() {
+		return "Eureka Client application";
 	}
 
 }
